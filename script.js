e433fc5920cc251c250e63e0a5331924d4a4f234
jahatesu <jannajustiniano1@gmail.com>
2025-08-29 14:39:08 +0800

    Added delete functionality and fixed no task display

--- script.js
+++ script.js
@@ -7,15 +7,46 @@ add.addEventListener("click", () => {
     if (task === "") {
         return;
     }else{
-        const newTask = document.createElement("div")
-        newTask.textContent = task;
+        const newTask = document.createElement("div");
+        newTask.classList.add("task-item");
+
+        const checkBox = document.createElement("input");
+        checkBox.addEventListener("change", () =>{
+            if (checkBox.checked) {
+                taskTextSpan.style.textDecoration = "line-through";
+            } else {
+                taskTextSpan.style.textDecoration = "none";
+            }
+        })
+        checkBox.type = "checkbox";
+        checkBox.classList.add("checkbox");
+
+        const taskTextSpan = document.createElement("span");
+        taskTextSpan.textContent = task;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.addEventListener("click", () => {
+            newTask.remove();
+
+            const tasks = taskList.querySelectorAll(".task-item");
+            if (tasks.length === 0) {
+                const noTask = document.querySelector("#no-tasks");
+                noTask.style.display = "block";
+                }
+        })
+
+        deleteBtn.textContent = "X";
+        deleteBtn.classList.add("delete-btn");
+
+        newTask.appendChild(checkBox);
+        newTask.appendChild(taskTextSpan);
+        newTask.appendChild(deleteBtn);
+
         taskList.appendChild(newTask);
 
         const noTask = document.querySelector("#no-tasks");
         noTask.style.display = "none";
         input.value = "";
         input.focus();
-
-        newTask.classList.add("task-item");
     }
 })
